1e827a27aac3dfa9ef5d9f799aebf6d8ea7ad269
Arturo Campos <info@arturocampos.dev>
2025-04-06 17:08:04 -0600

    chore: boilerplate

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import { ThemeProvider } from '@/components/theme-provider';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import '../styles/globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+export const metadata: Metadata = {
+  title: 'AI Fun',
+  description: 'Dress up with the help of AI',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable}`} suppressHydrationWarning>
+      <body className="antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          disableTransitionOnChange
+          enableSystem
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="grid bg-background min-h-screen">
+      <main className="relative mx-auto max-w-2xl px-6 pb-24 pt-[10vh] sm:pb-32 lg:px-10 w-full">
+        <h1 className="text-center font-medium text-4xl xl:text-5xl mt-2 sm:mt-4">
+          Welcome to AI Fun!
+        </h1>
+        <p className="text-muted-foreground mt-4 sm:mt-6 md:mt-8">
+          Ea laboris do exercitation adipisicing enim. Labore labore dolore
+          occaecat dolor amet fugiat eu nisi commodo nisi ullamco irure.
+          Consectetur ullamco minim sunt voluptate est minim officia nulla eu et
+          nisi proident veniam occaecat.
+        </p>
+      </main>
+    </div>
+  );
+}
